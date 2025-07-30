26a6f76d3453f1845a037b77b8410568674f03cf
0tieno <ronvi035@gmail.com>
2025-07-30 08:00:11 +0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+ 
+  return (
+     <h1 className="text-4xl md:text-5xl font-bold mb-4 text-blue-700">
+        Aga Khan Education Services Data Portal
+      </h1>
+  )
+}
+
+export default App
